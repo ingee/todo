e3246eae3006f6c6b6ef00ddd1b4b2fa23001a3b
unknown <1109320@SKT1109320PN002.SKT.AD>
2014-07-28 13:46:59 +0900

    add Model and Collection

---
+++ js/model/todo.js
@@ -0,0 +1,14 @@
+﻿var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+  default: {
+    title: '',
+    completed: false
+  },
+  
+  toogle: function(){
+    this.save({
+      completed: !this.get('completed')
+    });
+  }
+});
