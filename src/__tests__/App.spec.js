78aa1d87cbb25903d2852ba6fdb5621a8ad47af6
InGee Kim <ingee.kim@sk.com>
2023-05-11 10:47:52 +0900

    unit test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,11 +1,38 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, it, expect, vi } from 'vitest'
+import { shallowMount, flushPromises } from '@vue/test-utils'
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import { createTestingPinia } from '@pinia/testing'
+import merge from 'lodash.merge'
+import App from '@/App.vue'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+describe('App.vue', () => {
+  function createWrapper (option) {
+    const pinia = createTestingPinia({
+      initialState: option
+        ? {
+            todo: option.initialState,
+          }
+        : {},
+      createSpy: vi.fn,
+    })
+    const defaultOpt = {
+      global: {
+        plugins: [pinia],
+      },
+    }
+    return shallowMount(App, merge(defaultOpt, option))
+  }
+
+  it('shows todos in store', () => {
+    const todos = [
+      'wake up',
+      'wash up',
+      'go to work',
+    ]
+    const initialState = { todos }
+    const wrapper = createWrapper({ initialState })
+    for (const todo of todos) {
+      expect(wrapper.text()).toContain(todo)
+    }
   })
 })
