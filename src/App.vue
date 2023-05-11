9de0a8bdcd6b1b35df97aaabc50a21a7200e00ce
InGee Kim <ingee.kim@sk.com>
2023-05-11 14:09:57 +0900

    add todo item

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <h1>Todo</h1>
   <div>
+    <div>
+      <label for="todo">Enter todo</label>
+      <input type="text" data-test="todo-input" v-model="todo" @keyup.enter="onAdd" />
+      <button type="button" data-test="add-btn" @click="onAdd">
+        Add
+      </button>
+    </div>
     <ul>
       <li v-for="(todo, i) in store.todos" :key="i">
         {{ todo }}
@@ -10,6 +17,14 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useTodoStore } from '@/stores/todo'
+
+const todo = ref('')
 const store = useTodoStore()
+
+function onAdd () {
+  store.addTodo(todo.value)
+  todo.value = ""
+}
 </script>
