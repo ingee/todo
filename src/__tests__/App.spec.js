c4aabb36bc9e83ea17196ba256a7829f2b09a8e9
InGee Kim <ingee.kim@sk.com>
2023-05-11 17:35:19 +0900

    uit test

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,11 +1,18 @@
 import { describe, it, expect, vi } from 'vitest'
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, flushPromises } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import { useTodoStore } from '@/stores/todo'
 import merge from 'lodash.merge'
 import App from '@/App.vue'
 
 describe('App.vue', () => {
+  async function setStore (state) {
+    const store = useTodoStore()
+    store.$patch(state)
+    await flushPromises()
+    return store
+  }
+
   function createWrapper (option) {
     const pinia = createTestingPinia({
       initialState: option
@@ -48,5 +55,30 @@ describe('App.vue', () => {
 
     const store = useTodoStore()
     expect(store.addTodo).toBeCalledWith(todo)
+  })
+
+  it('removes todo-item when rm-btn is clicked', async () => {
+    const todo = 'something to do'
+    const wrapper = createWrapper({
+      initialState: {
+        todos: [ todo ],
+      },
+    })
+    let rmBtns = wrapper.findAll('[data-test="rm-btn"]')
+    expect(rmBtns.length).toBe(1)
+    await rmBtns.at(0).trigger('click')
+    let store = useTodoStore()
+    expect(store.rmTodo).toBeCalledWith(0)
+
+    const todos = [
+      'something #1',
+      'something #2',
+      'something #3',
+    ]
+    store = await setStore({ todos })
+    rmBtns = wrapper.findAll('[data-test="rm-btn"]')
+    expect(rmBtns.length).toBe(todos.length)
+    await rmBtns.at(1).trigger('click')
+    expect(store.rmTodo).toBeCalledWith(1)
   })
 })
